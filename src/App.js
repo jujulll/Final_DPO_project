e4fe5f1ea2e6998a4911e58f85f288359f56fe9b
Julia Ivshina <ivshinayv@gmail.com>
2026-05-27 18:39:54 +0500

    remove gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -13,7 +13,7 @@ import About from './pages/About'
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
     <ScrollToTop />
       <Header />
       <main>
@@ -28,7 +28,7 @@ function App() {
         </Routes>
       </main>
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
